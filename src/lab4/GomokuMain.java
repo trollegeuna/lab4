5e8fe2492d2a0ad6623d531130b5fab8fe5e4fdc
DavidRutqvist <david.rutqvist@bnearit.se>
2014-02-20 21:56:23 +0100

    Fixed GomokuMain

--- src/lab4/GomokuMain.java
+++ src/lab4/GomokuMain.java
@@ -7,7 +7,15 @@ import lab4.gui.GomokuGUI;
 public class GomokuMain {
 
 	public static void main(String[] args) {
-		GomokuClient client = new GomokuClient(Integer.parseInt(args[0]));
+		GomokuClient client;
+		if(args.length == 1)
+		{
+			client = new GomokuClient(Integer.parseInt(args[0]));
+		}
+		else
+		{
+			client = new GomokuClient(7000);
+		}
 		GomokuGameState gameState = new GomokuGameState(client);
 		GomokuGUI gui = new GomokuGUI(gameState, client);
 	}
