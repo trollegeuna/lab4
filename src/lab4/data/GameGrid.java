3d6ac8c142cc27a7af5c67db81dc0dc2cc05f775
Trolle <trolle.geuna@gmail.com>
2014-02-20 13:50:56 +0100

    fixade till med GameGrid. Skapade privata metoder för att kontrollera om någon fått 5 i rad. Tror att det stämmer men man kan aldrig vara helt säker. :) Ska starta på GomokuGameState senare idag.

--- src/lab4/data/GameGrid.java
+++ src/lab4/data/GameGrid.java
@@ -2,12 +2,21 @@ package lab4.data;
 
 import java.util.Observable;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 /**
  * Represents the 2-d game grid
  */
 
 public class GameGrid extends Observable{
-	public static int EMPTY, ME, OTHER;
+	/*
+	 * added ints and a 2 dim array and a int for counting rows
+	 */
+	public static final int EMPTY = 0;
+	public static final int ME = 1;
+	public static final int OTHER = 2;
+	public static final int INROW = 5;
+	private int[][] grid;
 
 	
 	/**
@@ -15,7 +24,9 @@ public class GameGrid extends Observable{
 	 * 
 	 * @param size The width/height of the game grid
 	 */
-	public GameGrid(int size){}
+	public GameGrid(int size){
+		grid = new int [size][size];
+	}
 	
 	/**
 	 * Reads a location of the grid
@@ -24,14 +35,18 @@ public class GameGrid extends Observable{
 	 * @param y The y coordinate
 	 * @return the value of the specified location
 	 */
-	public int getLocation(int x, int y){}
+	public int getLocation(int x, int y){
+		return grid[x][y];
+	}
 	
 	/**
 	 * Returns the size of the grid
 	 * 
 	 * @return the grid size
 	 */
-	public int getSize(){}
+	public int getSize(){
+		return grid.length;
+	}
 	
 	/**
 	 * Enters a move in the game grid
@@ -41,12 +56,33 @@ public class GameGrid extends Observable{
 	 * @param player
 	 * @return true if the insertion worked, false otherwise
 	 */
-	public boolean move(int x, int y, int player){}
+	public boolean move(int x, int y, int player){
+		if (grid[x][y] == EMPTY) {
+			grid[x][y] = player;
+			setChanged();
+			notifyObservers();
+			return true;
+		} else {
+			setChanged();
+			notifyObservers();
+			return false;
+
+		}
+	}
 	
 	/**
 	 * Clears the grid of pieces
 	 */
-	public void clearGrid(){}
+	public void clearGrid(){
+		int NUM = getSize();
+		for (int i = 0; i < NUM; i++) {
+			for (int j = 0; j < NUM; j++) {
+				grid[i][j] = EMPTY;
+			}
+		}
+		setChanged();
+		notifyObservers();
+	}
 	
 	/**
 	 * Check if a player has 5 in row
@@ -54,7 +90,103 @@ public class GameGrid extends Observable{
 	 * @param player the player to check for
 	 * @return true if player has 5 in row, false otherwise
 	 */
-	public boolean isWinner(int player){}
+	public boolean isWinner(int player){
+		if (rowCheck(player)) {
+			return true;
+		}else if (colCheck(player)) {
+			return true;
+		}else if (forwardDiagCheck(player)) {
+			return true;
+		}else if (backwardDiagCheck(player)) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	private boolean rowCheck(int player) {
+		
+		for (int x = 0; x < getSize(); x++) {
+			for (int y = 0; y < getSize(); y++) {
+				int counter = 0;
+				int i = x;
+				while (i < getSize() && i < getSize() && grid[i][y] == player) {
+					counter++;
+					i++;
+				}
+				if (counter >= INROW) {
+					return true;
+				}
+			}
+		}
+		return false;	
+		
+	}
+	
+	private boolean colCheck(int player) {
+		
+		for (int x = 0; x < getSize(); x++) {
+			
+			for (int y = 0; y < getSize(); y++) {
+				int counter = 0;
+				int j = y;
+				while (j < getSize() && j < getSize() && grid[x][j] == player) {
+					counter++;
+					j++;
+				}
+				if (counter >= INROW) {
+					return true;
+				}
+			}
+		}
+		return false;	
+	}
+	
+	private boolean forwardDiagCheck(int player) {
+	
+			
+		for (int x = 0; x < getSize(); x++) {
+			for(int y = 0; y < getSize(); y++){
+				
+				int counter = 0;
+				int i = x;
+				int j = y;
+				while (i < getSize() && j < getSize() && grid[i][j] == player) {
+					counter++;
+					i++;
+					j++;
+				}
+				if (counter >= INROW) {
+					return true;
+				} 
+			}
+		}
+		return false;
+		
+	}
+	
+	private boolean backwardDiagCheck(int player) {
+		
+		for (int x = 0; x < getSize(); x++) {
+			for(int y = 0; y < getSize(); y++){
+				
+				int counter = 0;
+				int i = x;
+				int j = y;
+				
+				while (i < getSize() && j < getSize() && i >= 0 && grid[i][j] == player) {
+					counter++;
+					i--;
+					j++;
+				}
+				if (counter >= INROW) {
+					return true;
+				} 
+			}
+		}
+		return false;
+		
+	}
 	
 	
 }
